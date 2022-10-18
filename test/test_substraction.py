5d4f6fcd20ee3861a05aba497069f0a0d4e330e8
dokajajob <dokajajob@gmail.com>
2022-10-18 16:59:34 +0300

    fixed multiply / substract tests

--- test/test_substraction.py
+++ test/test_substraction.py
@@ -1,8 +1,8 @@
 from src.calculator import subtract
 import pytest
 def test_subtract():
-    result = subtract(3, 4)
-    assert result == 7
+    result = subtract(8, 4)
+    assert result == 4
 def test_subtract_string():
     with pytest.raises(TypeError):
         subtract("string", 4)
